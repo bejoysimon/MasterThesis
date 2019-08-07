0897346b7d4054bcf2b6e4c37b5d94c45555cd2c
Bejoy Simon <bejoysimon@gmail.com>
2019-08-07 15:30:53 +0100

    commit: betting page in progress

---
+++ betting.py
@@ -0,0 +1,54 @@
+import webapp2
+import jinja2
+from google.appengine.api import users
+from google.appengine.ext import ndb
+import os
+
+from myuser import MyUser
+from userModel import UserModel
+from mySquad import MySquad
+
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True
+)
+
+class Betting(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/html'
+
+        username = self.request.get('username')
+
+        unique_key = ndb.Key('UserModel', username)
+        unique_user = unique_key.get()
+
+        squad_key = ndb.Key('MySquad', username)
+        squad = squad_key.get()
+
+        template_values = {'unique_user' : unique_user, 'squad' : squad}
+
+        template = JINJA_ENVIRONMENT.get_template('betting.html')
+        self.response.write(template.render(template_values))
+
+    def post(self):
+        self.response.headers['Content-Type'] = 'text/html'
+
+        user = users.get_current_user()
+        myuser_key = ndb.Key('MyUser', user.user_id())
+        myuser = myuser_key.get()
+
+        unique_key = ndb.Key('UserModel', myuser.username)
+        unique_user = unique_key.get()
+
+        action = self.request.get('button')
+
+        if action == "Logout":
+            self.redirect('/')
+
+
+        template_values = {'unique_user' : unique_user}
+
+        template = JINJA_ENVIRONMENT.get_template('betting.html')
+        self.response.write(template.render(template_values))
